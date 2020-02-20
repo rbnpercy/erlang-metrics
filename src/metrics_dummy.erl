edd6da6f95092e73d443f4749ac50750db09d65b
Robin Percy <robin@percy.pw>
2020-02-20 22:08:37 +0000

    Update metrics_dummy.erl

--- src/metrics_dummy.erl
+++ src/metrics_dummy.erl
@@ -1,10 +1,3 @@
-%%% -*- erlang -*-
-%%%
-%%% This file is part of metrics released under the BSD license.
-%%% See the LICENSE for more information.
-%%%
-%%% @doc dummy metric module
-%%%
 -module(metrics_dummy).
 
 
