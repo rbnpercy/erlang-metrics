b0acca8e794e1c3bffd3dfee13cf99e4b46d0ea9
Robin Percy <robin@percy.pw>
2020-02-20 22:08:54 +0000

    Update metrics_exometer.erl

--- src/metrics_exometer.erl
+++ src/metrics_exometer.erl
@@ -1,9 +1,3 @@
-%%% -*- erlang -*-
-%%%
-%%% This file is part of metrics released under the BSD license.
-%%% See the LICENSE for more information.
-%%%
-%%% @doc metrics module for exometer
 -module(metrics_exometer).
 
 
